66b62f501080cfa9fc61fcd057ca4ab8ece9b42c
Steve0x2a <stevesough@gmail.com>
2023-02-05 22:51:33 +0800

    fix: rough judgment in the case of ipv6

--- utils/ip_check.py
+++ utils/ip_check.py
@@ -3,6 +3,9 @@ import IPy
 
 def check_ip_if_internal(check_ip, ip_list):
     '''检查IP是否为公网IP&是否包含于排除的网段中'''
+    # IPv6 情况比较复杂， 目前只判断是否为fe80 开头，如果不是则判断属于公网，后续考虑判断两个ipv6在不在同一个网段。
+    if IPy.IP(check_ip).version() == 6 and not check_ip.startswith('fe80'):
+        return False
     if IPy.IP(check_ip).iptype() == "PUBLIC":
         return False
     for ip in ip_list:
